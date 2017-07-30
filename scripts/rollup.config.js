7617fb3c043703d8d41af8909be930a36c95ec7d
Lucas Azzola <derflatulator@gmail.com>
2017-07-30 23:11:00 +1000

    chore: enable uglify on dist generation (#11)

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,5 +1,7 @@
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
+import uglify from "rollup-plugin-uglify";
+import { minify } from "uglify-es";
 
 const targets = [
   { dest: "dist/styled-css-grid.esm.js", format: "es" },
@@ -11,6 +13,6 @@ export default {
   moduleName: "StyledCssGrid",
   external: ["styled-components", "react"],
   targets,
-  plugins: [commonjs(), babel()],
+  plugins: [commonjs(), babel(), uglify({}, minify)],
   globals: { "styled-components": "styled", react: "React" }
 };
